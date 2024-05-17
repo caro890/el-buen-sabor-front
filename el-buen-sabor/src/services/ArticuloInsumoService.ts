5a67ca519e0c8451853c4d8fc188272401d5d2b1
Luan <luanmollo@gmail.com>
2024-05-17 11:48:16 -0300

    corrijo rutas endpoints api

--- el-buen-sabor/src/services/ArticuloInsumoService.ts
+++ el-buen-sabor/src/services/ArticuloInsumoService.ts
@@ -2,7 +2,7 @@ import { ArticuloInsumo } from "../types/ArticuloInsumo";
 import { BackendClient } from "./BackendClient";
 
 export class ArticuloInsumoService extends BackendClient<ArticuloInsumo> {
-    baseUrl: string = "http://localhost:8080/articulos/insumos"; 
+    baseUrl: string = "http://localhost:8083/articulos/insumos"; 
 
     async getInsumosParaElaborar() {
         let url = this.baseUrl + "/buscar/elaborados";
