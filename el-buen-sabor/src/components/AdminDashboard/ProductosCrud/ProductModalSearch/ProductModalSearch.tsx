036d9b0fedeeefc48df9479c6defdf6d85ab3008
Carolina Solsona <carolina.solsona@alumnos.frm.utn.edu.ar>
2024-05-30 16:33:10 -0300

    corregi modal

--- el-buen-sabor/src/components/AdminDashboard/ProductosCrud/ProductModalSearch/ProductModalSearch.tsx
+++ el-buen-sabor/src/components/AdminDashboard/ProductosCrud/ProductModalSearch/ProductModalSearch.tsx
@@ -2,7 +2,7 @@ import { Button, Col, Modal, Row, Form, Container } from "react-bootstrap"
 import { FC, useEffect, useState } from "react";
 import "../../../../styles/ProductModalSearch.module.css"
 import { ArticuloInsumo } from "../../../../types/Articulos/ArticuloInsumo";
-import { useAppDispatch, useAppSelector } from "../../../../hooks/redux";
+import { useAppDispatch/*, useAppSelector */} from "../../../../hooks/redux";
 import { setIngredientes } from "../../../../redux/slices/IngredientesReducer";
 
 //valores que recibe el componente
@@ -14,8 +14,10 @@ interface IPropsProductModalSearch {
 
 export const ProductModalSearch : FC<IPropsProductModalSearch> = ({open, handleClose, options}) => {
   const [filteredData, setFilteredData] = useState<ArticuloInsumo[]>([]);  //estado para filtrar las opciones
+  const [insumos, setInsumos] = useState<ArticuloInsumo[]>([]); //estado para manejar los insumos seleccionados
+
   const dispatch = useAppDispatch();
-  const ingredientes = useAppSelector((state) => (state.ingredientesReducer.ingredientes));
+  //const ingredientes = useAppSelector((state) => (state.ingredientesReducer.ingredientes));
 
   //cuando cargo el componente, inicia la data a mostrar con las options que recibe por parametro
   useEffect(() => {
@@ -25,7 +27,7 @@ export const ProductModalSearch : FC<IPropsProductModalSearch> = ({open, handleC
   //funcion para cuando se marca o desmarca el checkbox de una opcion
   //recibo por parametro la opcion del checkbox que cambió y su propiedad checked
   const handleCheckButtonChange = (item: ArticuloInsumo, checked: boolean) => {
-    var aux: ArticuloInsumo[] = ingredientes.slice();   //copio el estado de insumo
+    var aux: ArticuloInsumo[] = insumos.slice();   //copio el estado de insumo
     //busco el item en el array auxiliar
     var f = 0;
     var found = aux.some(function(element, index) { f = index; return element.id == item.id; });
@@ -37,7 +39,13 @@ export const ProductModalSearch : FC<IPropsProductModalSearch> = ({open, handleC
         if (found) //y encuentro el item en el array
            aux.splice(f, 1);    //lo elimino del array auxiliar
     }
-    dispatch(setIngredientes(aux));    //seteo el estado detalle con el array auxiliar
+    setInsumos(aux);
+  };
+
+  const handleAdd = () => {
+    dispatch(setIngredientes(insumos));    //seteo el estado detalle con el array auxiliar
+    setInsumos([]);
+    handleClose();
   };
 
   //función para realizar la búsqueda de opciones por código
@@ -105,7 +113,7 @@ export const ProductModalSearch : FC<IPropsProductModalSearch> = ({open, handleC
         </Modal.Body>
         <Modal.Footer>
           <Button className="btnClose" type="button" onClick={handleClose}>CERRAR</Button> {/*Boton para cerrar la ventana */}
-          <Button className="btnAdd" type="button" onClick={handleClose}>AGREGAR</Button> {/*Boton para agregar los detalles elegidos*/}
+          <Button className="btnAdd" type="button" onClick={handleAdd}>AGREGAR</Button> {/*Boton para agregar los detalles elegidos*/}
         </Modal.Footer>
       </Modal>
   )
