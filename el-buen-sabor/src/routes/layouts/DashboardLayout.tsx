4c29fd5a2f2653990a87abe30d80b64adba47962
Carolina Solsona <carolina.solsona@alumnos.frm.utn.edu.ar>
2024-06-10 16:52:50 -0300

    redireccionar a admin-console

--- el-buen-sabor/src/routes/layouts/DashboardLayout.tsx
+++ el-buen-sabor/src/routes/layouts/DashboardLayout.tsx
@@ -1,22 +1,22 @@
-import { Outlet/*, useNavigate*/ } from "react-router"
+import { Outlet, useNavigate } from "react-router"
 import BasicSidebar from "../../components/AdminDashboard/BasicSidebar/BasicSidebar"
 import '../../styles/AdminDashboard.css'
-//import { useEffect } from "react";
-//import { useAppSelector } from "../../hooks/redux";
+import { useEffect } from "react";
+import { useAppSelector } from "../../hooks/redux";
 
 export const DashboardLayout = () => {
   //obtengo la empresa actual del estado global
-  /*const empresaActual = useAppSelector((state) => state.empresaReducer.empresa);
+  const empresaActual = useAppSelector((state) => state.empresaReducer.empresa);
   //obtengo la sucursal actual del estado global
   const sucursalActual = useAppSelector((state) => state.empresaReducer.activeSucursal);
-  const navigate = useNavigate(); */
+  const navigate = useNavigate(); 
 
-  /*useEffect(() => {   
+  useEffect(() => {   
     //cuando cargo el dashboard me aseguro de que haya una empresa y una sucursal seleccionadas
     if(empresaActual === null && sucursalActual === null){ 
       navigate("/admin-console"); //si no hay ninguna seleccionada redirecciono al la consola para seleccionarlas
     }
-  }, []);*/
+  }, []);
 
   return (
     <>
