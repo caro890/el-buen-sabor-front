bfb2c5a89e9c4a263b1db937a663ee328581e646
Carolina Solsona <carolina.solsona@alumnos.frm.utn.edu.ar>
2024-06-24 23:32:01 -0300

    Nuevos endpoints estadisticas

--- el-buen-sabor/src/components/AdminDashboard/Estadisticas/Estadisticas.tsx
+++ el-buen-sabor/src/components/AdminDashboard/Estadisticas/Estadisticas.tsx
@@ -1,18 +1,20 @@
 import { Box, Typography } from "@mui/material";
 import { RankingProductosModule } from "./Charts/RankingProductos";
+import { Container } from "react-bootstrap";
 
 export const Estadisticas = () => {
   return (
     <Box component="main" sx={{ flexGrow: 1, my: 2 }}>
-      <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", my: 1 }}>
-        <Typography variant="h5" gutterBottom>
-          Estadísticas
-        </Typography>
-      </Box>
-      <Box>
-        <RankingProductosModule business="sucursal"/>
-        <RankingProductosModule business="empresa"/>
-      </Box>
+      <Container>
+        <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", my: 1 }}>
+          <Typography variant="h5" gutterBottom>
+            Estadísticas Empresa
+          </Typography>
+        </Box>
+        <Box>
+          <RankingProductosModule business="empresa"/>
+        </Box>
+      </Container>
     </Box>
   )
 }
